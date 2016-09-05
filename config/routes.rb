de0087566d958c4f5ca458cd0c4687b492371e61
Zach Warren <zwarren2@gmail.com>
2016-09-05 16:35:13 -0400

    Adding posts and updating root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
-  root 'application#index'
+  #root 'users#index'
+  #the format of the root location is based on the app controller title.  users_controller.rb not application_controller.rb.  
+  root 'microposts#index'
 
 end
